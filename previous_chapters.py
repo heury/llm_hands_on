5a2e64a4b5677b76203101300dcbf78397e16f5d
heury <heury@naver.com>
2025-12-29 21:44:22 +0900

    loss 계산 오류 수정

--- previous_chapters.py
+++ previous_chapters.py
@@ -603,17 +603,14 @@ def calc_accuracy_loader(data_loader, model, device, num_batches=None):
             break
     return correct_predictions / num_examples
 
-
-# def calc_loss_batch(input_batch, target_batch, model, device):
-#     input_batch, target_batch = input_batch.to(device), target_batch.to(device)
-#     logits = model(input_batch)[:, -1, :]  # Logits of last output token
-#     loss = torch.nn.functional.cross_entropy(logits, target_batch)
-#     return loss
-
 def calc_loss_batch(input_batch, target_batch, model, device):
     input_batch, target_batch = input_batch.to(device), target_batch.to(device)
     logits = model(input_batch)
-    loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
+    # target이 1차원이면 마지막 토큰만 , 2차원이면 전체 문장
+    if target_batch.dim() == 1:
+        loss = torch.nn.functional.cross_entropy(logits[:, -1, :], target_batch)
+    else:
+        loss = torch.nn.functional.cross_entropy(logits.flatten(0, 1), target_batch.flatten())
     return loss
 
 # Overall the same as `train_model_simple` in chapter 5
@@ -654,6 +651,43 @@ def train_classifier_simple(model, train_loader, val_loader, optimizer, device, 
 
     return train_losses, val_losses, train_accs, val_accs, examples_seen
 
+# Overall the same as `train_model_simple` in chapter 5
+def train_classifier_lora(model, train_loader, val_loader, optimizer, device, num_epochs,
+                            eval_freq, eval_iter):
+    # Initialize lists to track losses and tokens seen
+    train_losses, val_losses, train_accs, val_accs = [], [], [], []
+    examples_seen, global_step = 0, -1
+
+    # Main training loop
+    for epoch in range(num_epochs):
+        model.train()  # Set model to training mode
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()  # Reset loss gradients from previous batch iteration
+            loss = calc_loss_batch_lora(input_batch, target_batch, model, device)
+            loss.backward()  # Calculate loss gradients
+            optimizer.step()  # Update model weights using loss gradients
+            examples_seen += input_batch.shape[0]  # New: track examples instead of tokens
+            global_step += 1
+
+            # Optional evaluation step
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+
+        # Calculate accuracy after each epoch
+        train_accuracy = calc_accuracy_loader(train_loader, model, device, num_batches=eval_iter)
+        val_accuracy = calc_accuracy_loader(val_loader, model, device, num_batches=eval_iter)
+        print(f"Training accuracy: {train_accuracy*100:.2f}% | ", end="")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+
+    return train_losses, val_losses, train_accs, val_accs, examples_seen
 
 def plot_values(epochs_seen, examples_seen, train_values, val_values, label="loss"):
     fig, ax1 = plt.subplots(figsize=(5, 3))
