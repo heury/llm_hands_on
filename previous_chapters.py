6d87d956bdb233e0070d19d2282db822f273b7e3
heury <heury@naver.com>
2025-12-30 09:51:38 +0900

    data 경로 수정,  plot_losses  수정

--- previous_chapters.py
+++ previous_chapters.py
@@ -708,15 +708,15 @@ def plot_values(epochs_seen, examples_seen, train_values, val_values, label="los
     plt.savefig(f"outputs/{label}-plot.pdf")
     plt.show()
 
-def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses, label="loss"):
     fig, ax1 = plt.subplots(figsize=(5, 3))
 
     # Plot training and validation loss against epochs
-    ax1.plot(epochs_seen, train_losses, label="Training loss")
-    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.plot(epochs_seen, train_losses, label=f"Training {label}")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label=f"Validation {label}")
     ax1.set_xlabel("Epochs")
-    ax1.set_ylabel("Loss")
-    ax1.legend(loc="upper right")
+    ax1.set_ylabel(label.capitalize())
+    ax1.legend()
     ax1.xaxis.set_major_locator(MaxNLocator(integer=True))  # only show integer labels on x-axis
 
     # Create a second x-axis for tokens seen
@@ -725,5 +725,5 @@ def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
     ax2.set_xlabel("Tokens seen")
 
     fig.tight_layout()  # Adjust layout to make room
-    plt.savefig("outputs/loss-plot.pdf")
+    plt.savefig(f"outputs/{label}-plot.pdf")
     plt.show()
